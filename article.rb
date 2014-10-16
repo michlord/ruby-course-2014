3cade342c438aa874d5c1e6f0d98d7e7b6fb50ff
Michał Zimniak <zimniak.michal@gmail.com>
2014-10-16 19:10:00 +0200

    [#4] Created Article class.

---
+++ article.rb
@@ -0,0 +1,38 @@
+class Article
+  def initialize(title, content, author = '')
+    @title = title
+    @content = content
+    @author = author
+    @likes = 0
+    @dislikes = 0
+    @created_at = Time.now
+  end
+  def like!
+    @likes += 1
+  end
+  def dislike!
+    @dislikes += 1
+  end
+  def positive_votes
+    @likes - @dislikes
+  end
+  def votes
+    @likes + @dislikes
+  end
+  def shortened_to(limit)
+    if limit >= @content.length
+      @content
+    else
+      @content[0...limit - 3] + '...'
+    end
+  end
+  def include?(item)
+    @content.include?(item)
+  end
+  def words
+    @content.split(/\W+/)
+  end
+  def distinct_words
+    words.uniq
+  end
+end
