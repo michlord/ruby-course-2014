74453910030c92598d8cfb7962195e72e40a4db9
Michal Zimniak <zimniak.michal@gmail.com>
2014-10-20 15:07:39 +0200

    [#10] In TestArticleManager. Added empty lines between defs.

--- spec/unit/article_manager_spec.rb
+++ spec/unit/article_manager_spec.rb
@@ -29,24 +29,30 @@ class TestArticleManager < Test::Unit::TestCase
     
     @article_manager = ArticleManager.new([@article_1, @article_2, @article_3, @article_4])
   end
+
   def test_worst_articles
     assert_equal([@article_2, @article_3, @article_1, @article_4],
                   @article_manager.worst_articles)
   end
+
   def test_best_articles
     assert_equal([@article_4, @article_1, @article_3, @article_2],
-                      @article_manager.best_articles)
+                  @article_manager.best_articles)
   end
+
   def test_worst_article
     assert_equal(@article_2, @article_manager.worst_article)
   end
+
   def test_best_article
     assert_equal(@article_4, @article_manager.best_article)
   end
+
   def test_most_popular_article
     assert(@article_3 == @article_manager.most_popular_article ||
            @article_4 == @article_manager.most_popular_article)
   end
+
   def test_include?
     assert(@article_manager.include?("need"))
     assert(@article_manager.include?("collapse"))
@@ -61,16 +67,20 @@ class TestArticleManager < Test::Unit::TestCase
     assert(@article_manager.include?("_h") == false)
     assert(@article_manager.include?("!") == false)
   end
+
   def test_authors
     assert_equal(["Officer Nasty", "The Rufmeister General", "Ninja"],
                   @article_manager.authors)
   end
+
   def test_number_of_authors
     assert_equal(3, @article_manager.number_of_authors)
   end
+
   def test_votes
     assert_equal(36, @article_manager.votes)
   end
+
   def test_to_s
     str = "0 title: This feels dirty content: Play it on a mac and the world would collapse. author: Officer Nasty\n" +
     "1 title: The future is now! content: Now all you need is a PC emulator and you've pretty much got it all. author: The Rufmeister General\n" +
