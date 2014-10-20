6090229b14eb32b343ef0c1d7c28bca67659c0ac
Michal Zimniak <zimniak.michal@gmail.com>
2014-10-20 15:44:44 +0200

    [#6] In MockArticle. Use sample for random_word. Move initialize to the top of the class definition. Removed range from rand.

--- mock_article.rb
+++ mock_article.rb
@@ -2,30 +2,36 @@ require_relative  'article'
 
 class MockArticle < Article
   WORD_LIST = ["about","after","again","air","all",
-  "along","also","an","and","another",
-  "any","are","around","as","at","away",
-  "back","be","because","been","before",
-  "below","between","both","but","by",
-  "came","can","come","could","day",
-  "did","different","do","does","dont",
-  "down","each","end","even","every",
-  "few","find","first","for","found",
-  "from","get","give","go","good","great",
-  "had","has","have","he","help","her",
-  "here","him","his","home","house","how",
-  "I","if","in","into","is","it",
-  "its","just","know","large","last","left",
-  "like","line","little","long","look","made",
-  "make","man","many","may","me","men",
-  "might","more","most","Mr","must","my",
-  "name","never","new","next","no","not"]
+               "along","also","an","and","another",
+               "any","are","around","as","at","away",
+               "back","be","because","been","before",
+               "below","between","both","but","by",
+               "came","can","come","could","day",
+               "did","different","do","does","dont",
+               "down","each","end","even","every",
+               "few","find","first","for","found",
+               "from","get","give","go","good","great",
+               "had","has","have","he","help","her",
+               "here","him","his","home","house","how",
+               "I","if","in","into","is","it",
+               "its","just","know","large","last","left",
+               "like","line","little","long","look","made",
+               "make","man","many","may","me","men",
+               "might","more","most","Mr","must","my",
+               "name","never","new","next","no","not"]
+
+  def initialize
+    super(generate_title, generate_content, generate_author)
+    @likes = random_number
+    @dislikes = random_number
+  end
 
   def self.random_word
-    WORD_LIST[rand(0..WORD_LIST.length - 1)]
+    WORD_LIST.sample
   end
 
   def random_number
-    rand(0..100)
+    rand 100
   end
 
   def generate_author
@@ -49,12 +55,6 @@ class MockArticle < Article
     content = generate_sentence
     rand(0..4).times { content += " " + generate_sentence}
     content
-  end
-
-  def initialize
-    super(generate_title, generate_content, generate_author)
-    @likes = random_number
-    @dislikes = random_number
   end
   
   private_class_method :random_word
