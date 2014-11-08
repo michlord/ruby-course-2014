10c71fdd4c171daac0f8b3c81a52a2042a2da63d
Michał Zimniak <zimniak.michal@gmail.com>
2014-11-08 17:58:13 +0100

    [#17] MockArticle generation functions extracted into a gem.

--- mock_article.rb
+++ mock_article.rb
@@ -1,62 +1,13 @@
 require_relative  'article'
+require 'bla-bla'
 
 class MockArticle < Article
-  WORD_LIST = ["about","after","again","air","all",
-               "along","also","an","and","another",
-               "any","are","around","as","at","away",
-               "back","be","because","been","before",
-               "below","between","both","but","by",
-               "came","can","come","could","day",
-               "did","different","do","does","dont",
-               "down","each","end","even","every",
-               "few","find","first","for","found",
-               "from","get","give","go","good","great",
-               "had","has","have","he","help","her",
-               "here","him","his","home","house","how",
-               "I","if","in","into","is","it",
-               "its","just","know","large","last","left",
-               "like","line","little","long","look","made",
-               "make","man","many","may","me","men",
-               "might","more","most","Mr","must","my",
-               "name","never","new","next","no","not"]
-
   def initialize
-    super(generate_title, generate_content, generate_author)
-    @likes = random_number
-    @dislikes = random_number
-  end
-
-  def self.random_word
-    WORD_LIST.sample
+    super(BlaBla.title, BlaBla.content, BlaBla.author)
+    @likes, @dislikes = random_number, random_number
   end
 
   def random_number
     rand 100
   end
-
-  def generate_author
-    MockArticle.random_word.capitalize + " " + 
-      MockArticle.random_word.capitalize
-  end
-
-  def generate_title
-    title = MockArticle.random_word.capitalize
-    rand(0..4).times { title += " " + MockArticle.random_word }
-    title
-  end
-
-  def generate_sentence
-    sentence = MockArticle.random_word.capitalize
-    rand(4..9).times { sentence += " " + MockArticle.random_word }
-    sentence + "."
-  end
-
-  def generate_content
-    content = generate_sentence
-    rand(0..4).times { content += " " + generate_sentence}
-    content
-  end
-  
-  private_class_method :random_word
 end
-
--- spec/unit/mock_article_spec.rb
+++ spec/unit/mock_article_spec.rb
@@ -1,18 +1,8 @@
 require_relative '../../mock_article'
 
-class MockArticle
-  public_class_method  :random_word
-end
-
 class MockArticleSpec < Test::Unit::TestCase
   def setup
     @mock_article = MockArticle.new
-  end
-
-  def test_random_word
-    random_words = []
-    10.times { random_words << MockArticle.random_word }
-    assert(random_words.all? { |word| MockArticle::WORD_LIST.include?(word) })
   end
 
   def test_random_number
@@ -20,52 +10,6 @@ class MockArticleSpec < Test::Unit::TestCase
       number = @mock_article.random_number
       assert(number >= 0)
       assert(number <= 100)
-    end
-  end
-
-  def test_generate_author
-    10.times do
-      author = @mock_article.generate_author.split(/\W+/)
-      assert_equal(2, author.length)
-      first, last = author
-      assert_equal(first.capitalize, first)
-      assert_equal(last.capitalize, last)
-    end
-  end
-
-  def test_generate_title
-    20.times do
-      title = @mock_article.generate_title
-      words = title.split(/\W+/)
-      assert(words.length >= 1)
-      assert(words.length <= 5)
-      assert_equal(words[0].capitalize, words[0])
-    end
-  end
-
-  def test_generate_sentence
-    20.times do
-      sentence = @mock_article.generate_sentence
-      words = sentence.split(/\W+/)
-      assert(words.length >= 5)
-      assert(words.length <= 10)
-      assert_equal(words[0].capitalize, words[0])
-    end
-  end
-
-  def test_generate_content
-    20.times do
-      content = @mock_article.generate_content
-      # There must be a space after each period
-      # except the last one.
-      sentences = content.split(". ")
-      # Make sure there are no extra spaces
-      # around a sentence.
-      sentences.each do |s|
-        assert_equal(s.strip, s)
-      end
-      assert(sentences.length >= 1)
-      assert(sentences.length <= 5)
     end
   end
 end
